c6402264f079bee5fd84a44b61edb6731a1ebcd6
void -v <dyllanmccreary@gmail.com>
2024-10-16 11:44:55 +0000

    some more structures

--- crack.py
+++ crack.py
@@ -1,13 +1,28 @@
 
 
-t4 = "?OBKRUOXOGHULBSOLIFBBWEASTNORTHEASTOTWTQSJQSSEKZZWATJKLUDIAWINFBBERLINMZFPKWGDKZXTJCDIGKUHUAUEKCAR"
+k4 = "?OBKRUOXOGHULBSOLIFBBWEASTNORTHEASTOTWTQSJQSSEKZZWATJKLUDIAWINFBBERLINMZFPKWGDKZXTJCDIGKUHUAUEKCAR"
 
 # same text but laid out like the video https://www.youtube.com/watch?v=jVpsLMCIB0Y
-t4_layout = """
+k4_layout = """
                           ?OBKR
 UOXOGHULBSOLIFBBWEASTNORTHEASTO
 TWTQSJQSSEKZZWATJKLUDIAWINFBBER
 LINMZFPKWGDKZXTJCDIGKUHUAUEKCAR
 """
 
-print(t4_layout)
+k4_clock_layout = """
+                          ?OBKR
+UOXOGHULBSOLIFBBWEASTNORTHEASTO
+TWTQSJQSSEKZZWATJKLUDIAWINFBBER
+LINCLOCKWGDKZXTJCDIGKUHUAUEKCAR
+"""  # MZFPK gets mapped to CLOCK (hint from Jim Sanborn)
+
+print(k4_layout)
+
+# im thinking maybe we replace 
+# EAST with KITA
+# NORTH with (KOREA)? or maybe (TOZAI)
+# EAST with KITA (again)
+# CLOCK to (TOKEI)?
+
+# basically, we can define the cipher key with the alignemnt
