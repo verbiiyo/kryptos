a76839242b07c52977c5f850abf5f9dff1c9ab16
void -v <dyllanmccreary@gmail.com>
2024-10-17 19:15:42 +0000

    we got K2 decrypted!

--- crack.py
+++ crack.py
@@ -25,7 +25,7 @@ def wrap_decypher(text: str, wrap_key: int):
     return out_str
 
 
-def vigenere_decipher(text: str, key1: str, key2: str):
+def vigenere_decipher(cipher_text: str, key1: str, key2: str):
     # first key is used to construct the vigenere table
     # vigenere table is where you take the key1 and pull it all to the beginning
     # of the standard 26 letter alphabet.
@@ -33,20 +33,32 @@ def vigenere_decipher(text: str, key1: str, key2: str):
     # then, key2 can decipher the text using the vigenere table
     # by finding the row of the key2 character and the column of the text character
 
+    cipher_text = cipher_text.replace("\n", "")
+
     # let's begin
     # first, construct the vigenere table
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?"
     table = ""
-
     for char in key1:
         table += char
         alphabet = alphabet.replace(char, "")
-
     table += alphabet
 
-    print(table)
+    # encode the cipher text as alphabet indices in a list
+    cipher_indices = [table.index(char) for char in cipher_text]
 
-    exit()
+    # encode the key2 as alphabet indices in a list
+    key2_indices = [table.index(char) for char in key2]
+
+    # make key2_indices the same length as the cipher text by repeating it
+    key2_indices = key2_indices * (len(cipher_text) // len(key2_indices)) + key2_indices[:len(cipher_text) % len(key2_indices)]
+
+    # now, decipher the text by subtracting the key2 indices from the cipher text indices
+    out_str = ""
+    for i in range(len(cipher_text)):
+        out_str += table[(cipher_indices[i] - key2_indices[i]) % 27]
+    
+    return out_str
 
 
 
@@ -57,8 +69,9 @@ print(f"K3 has {len(K3.replace('\n', ''))} characters")
 print(f"K4 has {len(K4.replace('\n', ''))} characters")
 print()
 
-print("K1 decrypted:")
-print(vigenere_decipher(K1, "KRYPTOS", "PALIMPSEST"))
+print("K2 decrypted:")
+print(K2)
+print(vigenere_decipher(K2, "KRYPTOS", "ABSCISSA"))
 
 
 # we can decrypt K3 with wrap decypher using 192 as the key
