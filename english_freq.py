8405ad7a4eef037d9c86dbca206e0aeb8a6614ee
void -v <dyllanmccreary@gmail.com>
2024-10-17 16:54:12 +0000

    freq decrypt

--- english_freq.py
+++ english_freq.py
@@ -1,7 +1,7 @@
-from crack import plot_char_frequencies, get_char_frequencies
+from crack import plot_char_frequencies, get_char_frequencies, get_char_frequencies, k4_no_spaces
 
 
-plot_char_frequencies("""
+random_english_text = """
 
 Why Countries Use Different Timezones: A Global Necessity
 
@@ -35,7 +35,24 @@ Similarly, some countries use multiple time zones to reflect regional autonomy o
 Conclusion
 Countries use different time zones primarily to align daily activities with the Earth's rotation, maximizing the use of daylight for societal, economic, and environmental benefits. These time zones facilitate global coordination, ensuring that international trade, communication, and travel can occur efficiently across the world. Adjustments such as daylight saving time and the influence of political decisions further shape time zone usage, reflecting the complex ways in which human societies adapt to the movement of the Earth and the demands of modern life. Time zones, while seemingly simple, are essential to the smooth functioning of a globalized world.
 
-""")
+"""
+
+plot_char_frequencies(random_english_text)
 
 
+char_counts, sorted_chars = get_char_frequencies(random_english_text)
+
+def frequency_decrypt(text: str):
+    # basically, let's decrypt by matching up the frequency sorts as our key
+    # and then just replace the characters
+    input_char_counts, input_sorted_chars = get_char_frequencies(text)
+
+    # the key is from the input sorted chars to the sorted chars
+    key = {input_char: char for input_char, char in zip(input_sorted_chars, sorted_chars)}
+
+    # decrypt the text
+    decrypted_text = "".join([key.get(char, char) for char in text])
+    return decrypted_text
+
+print(frequency_decrypt(k4_no_spaces))
 
