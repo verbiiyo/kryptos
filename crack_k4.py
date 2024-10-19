dff94c19ae1733460441c9f6483949b2a4b2274e
void -v <dyllanmccreary@gmail.com>
2024-10-19 10:14:46 +0000

    better spacing

--- crack_k4.py
+++ crack_k4.py
@@ -21,5 +21,5 @@ def get_mismatch_chars() -> Tuple[List[int], List[str], List[str]]:
 if __name__ == "__main__":
     mis_indices, mis_k4_chars, mis_hint_chars = get_mismatch_chars()
     print(f"Indices:\t{" ".join([str(i) for i in mis_indices])}")
-    print(f"K4 Chars:\t{"".join(mis_k4_chars)}")
-    print(f"Hint Chars:\t{"".join(mis_hint_chars)}")
+    print(f"K4 Chars:\t{"  ".join(mis_k4_chars)}")
+    print(f"Hint Chars:\t{"  ".join(mis_hint_chars)}")
