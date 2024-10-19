2b47eaca797bd688486c45044b32256d02fae629
void -v <dyllanmccreary@gmail.com>
2024-10-19 10:18:13 +0000

    we had some missing chars (that matched)

--- crack_k4.py
+++ crack_k4.py
@@ -4,14 +4,18 @@ from crack import vigenere_decipher, multi_wrap_decipher, wrap_decypher
 from messages import KRYPTOS, K1, K2, K3, K4, K4_WITH_HINTS
 
 
-def get_mismatch_chars() -> Tuple[List[int], List[str], List[str]]:
+def get_hint_chars() -> Tuple[List[int], List[str], List[str]]:
     indices = []
     k4_chars = []
     hint_chars = []
 
+    # oh, also, there are some places where they do match...
+    # so for those, we will automatically add them as well.
+    matching_char_indices = [32, 73]
+
     # let's print the mismatch characters and their indices
     for i, (k4_char, k4_hint_char) in enumerate(zip(K4, K4_WITH_HINTS)):
-        if k4_char != k4_hint_char:
+        if k4_char != k4_hint_char or i in matching_char_indices:
             indices.append(i)
             k4_chars.append(k4_char)
             hint_chars.append(k4_hint_char)
@@ -19,7 +23,7 @@ def get_mismatch_chars() -> Tuple[List[int], List[str], List[str]]:
     return indices, k4_chars, hint_chars
 
 if __name__ == "__main__":
-    mis_indices, mis_k4_chars, mis_hint_chars = get_mismatch_chars()
-    print(f"Indices:\t{" ".join([str(i) for i in mis_indices])}")
-    print(f"K4 Chars:\t{"  ".join(mis_k4_chars)}")
-    print(f"Hint Chars:\t{"  ".join(mis_hint_chars)}")
+    hint_indices, no_hint_chars, hint_chars = get_hint_chars()
+    print(f"Indices:\t{" ".join([str(i) for i in hint_indices])}")
+    print(f"K4 Chars:\t{"  ".join(no_hint_chars)}")
+    print(f"Hint Chars:\t{"  ".join(hint_chars)}")
