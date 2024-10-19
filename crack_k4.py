6b914e4baf537ea493268e460fd018f3767ffa8f
void -v <dyllanmccreary@gmail.com>
2024-10-19 10:30:31 +0000

    try_all_possible_berlin_clock_combinations

--- crack_k4.py
+++ crack_k4.py
@@ -22,8 +22,26 @@ def get_hint_chars() -> Tuple[List[int], List[str], List[str]]:
 
     return indices, k4_chars, hint_chars
 
+
+def try_all_possible_berlin_clock_combinations():
+    for i in range(2):
+        for j in range(5):
+            for k in range(12):
+                for l in range(5):
+                    print("Trying", i, j, k, l)
+                    multi_wrap_decipher(K4, [i, j, k, l])
+
+
 if __name__ == "__main__":
     hint_indices, no_hint_chars, hint_chars = get_hint_chars()
     print(f"Indices:\t{" ".join([str(i) for i in hint_indices])}")
     print(f"K4 Chars:\t{"  ".join(no_hint_chars)}")
     print(f"Hint Chars:\t{"  ".join(hint_chars)}")
+
+    try_all_possible_berlin_clock_combinations()
+
+    # for i in range(100):
+    #     for j in range(100):
+    #         for k in range(100):
+    #             print("Trying", i, j, k)
+    #             multi_wrap_decipher(K4, [i, j, k])
